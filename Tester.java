732e2d56b5072caeafc04083f4776092df323dd6
Asher Engelberg <ashercengelberg@icloud.com>
2023-08-30 12:08:56 -0700

    Day 2 Assignment
    Added the tester and count character method.

---
+++ Tester.java
@@ -0,0 +1,7 @@
+import java.io.IOException;
+
+public class Tester {
+    public static void main(String[] args) throws IOException {
+        System.out.println(FileUtils.countCharacters("newFile.txt"));
+    }
+}
